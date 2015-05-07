c69f405c535e6cbec91e74fcd7d223c004500a43
weiyong <weiyong.me@qq.com>
2015-05-07 13:05:01 +0800

    11

---
+++ temp/static_caches/cat_option_static.php
@@ -0,0 +1,52 @@
+<?php
+$data = array (
+  29 => 
+  array (
+    'cat_id' => '29',
+    'cat_name' => '圣诞',
+    'measure_unit' => '100',
+    'parent_id' => '0',
+    'is_show' => '0',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '1',
+    'level' => 0,
+    'id' => '29',
+    'name' => '圣诞',
+  ),
+  26 => 
+  array (
+    'cat_id' => '26',
+    'cat_name' => '特色产品',
+    'measure_unit' => '',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '2',
+    'level' => 0,
+    'id' => '26',
+    'name' => '特色产品',
+  ),
+  28 => 
+  array (
+    'cat_id' => '28',
+    'cat_name' => '核心产品',
+    'measure_unit' => '',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '3',
+    'level' => 0,
+    'id' => '28',
+    'name' => '核心产品',
+  ),
+);
+?>
